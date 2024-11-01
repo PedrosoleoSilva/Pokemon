05869ee4a40e625e4ba80823329f8fade1cba364
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2024-11-01 13:46:55 -0300

    Criando footer

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import HeaderInicio from './components/HeaderInicio';
 import ListaPokemon from './components/ListaPokemon';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <HeaderInicio/>
       <ListaPokemon/>
+      <Footer/>
     </div>
   );
 }
---
+++ pokemon/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css"; 
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>Temos que pegar todos! Pokémon</p>
+                <p>Developed Leonardo</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
